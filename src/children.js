099ee7605af78c404cdebe7cd1c8ffcc0c2995ee
Chris Ford <chrisford51@gmail.com>
2019-07-30 23:05:43 -0400

    Added children.js

---
+++ src/children.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+export const children = [
+  {
+    href: "/images/mediaLibrarySnip.png",
+    title: "Media Library",
+    id: 0
+  },
+
+  {
+    href: "/images/loopLABSnip.png",
+    title: "LoopLAB",
+    id: 1
+  },
+
+  {
+    href: "/images/ralphWaldoEmersonSnip.png",
+    title: "Ralph Waldo Emerson",
+    id: 2
+  }
+]
